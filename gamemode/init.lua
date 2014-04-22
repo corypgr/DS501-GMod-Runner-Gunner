da75fcf626d3af11a78ba8985dcb8851f67ba592
Cory Groom <corypgr@hotmail.com>
2014-04-21 23:50:28 -0500

    Added some tests with HL2 zombies. It spawns 200 zombies around you, which start to attack whatever hurts them.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -9,3 +9,31 @@ function GM:PlayerSpawn( ply )
 	player_manager.SetPlayerClass( ply, "player_gunner" )
 	BaseClass.PlayerSpawn( self, ply )
 end
+
+function GM:InitPostEntity()
+	for i = 0,200,1 do
+		local zombie = ents.Create( "npc_zombie" )
+		zombie:SetPos( Vector(math.random(-1000,1000), math.random(-1000,1000), 500 ) )
+		zombie:Spawn()
+		-- Make the zombie ignore the players
+		zombie:AddRelationship( "player D_NU 10" )
+	end
+	print( "All Entities have initialized\n" )
+end
+
+hook.Add( "InitPostEntity", "zombie_spawn", function()
+	print( "Initialization hook called" )
+end )
+
+function GM:EntityTakeDamage( ent, dmgInfo )
+
+    local attacker = dmgInfo:GetAttacker()
+	if ent:IsNPC() and ent:GetEnemy() != attacker then
+		-- The zombie will now only attack the player that shot it
+		ent:AddEntityRelationship(attacker, D_HT, 99 )
+		ent:SetEnemy(attacker)
+		-- This lets the zombie find you faster
+		ent:UpdateEnemyMemory( attacker, attacker:GetPos() )
+		print("relationship changed")
+	end
+end
