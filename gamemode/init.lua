8846dbef73d424aa09c660552ee8dc675182abf7
Cory Groom <corypgr@hotmail.com>
2014-04-29 03:19:03 -0500

    added ability to make npc walk around randomly. Also, mapped e to make the gun show up in players hand. NPC gun shows up when you shoot them. Eliminates issue where npcs dont walk around with their guns up.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -5,20 +5,32 @@ include("shared.lua")
 
 DEFINE_BASECLASS( "gamemode_base" )
 
+util.PrecacheModel( "models/alyx.mdl" )
 function GM:PlayerSpawn( ply )
-	player_manager.SetPlayerClass( ply, "player_gunner" )
+	player_manager.SetPlayerClass( ply, "player_runner" )
+	ply:SetModel("models/alyx.mdl")
 	BaseClass.PlayerSpawn( self, ply )
 end
 
 function GM:InitPostEntity()
-	for i = 0,10,1 do
-		for k = 0,10,1 do
+	for i = 0,3,1 do
+		for k = 0,3,1 do
 			local zombie = ents.Create( "npc_alyx" )
 			zombie:SetPos( Vector(60*i, 60*k, 0 ) )
 			zombie:Spawn()
 			-- Make the zombie ignore the players
 			zombie:AddRelationship( "player D_NU 10" )
-			zombie:Give("weapon_alyxgun")
+			hook.Add("Think", "NPCThink " .. tostring(zombie), function()
+				if(zombie:GetMovementActivity() == -1 and 
+				  (not zombie:IsCurrentSchedule(SCHED_NONE) or math.random(1,10) == 1)) then
+					local poss = zombie:GetPos()
+					poss:Add(Vector(math.random(-500,500),math.random(-500,500),0))
+					zombie:SetLastPosition(poss) 
+					-- Note that SCHED_FORCED_GO and SCHED_FORCED_GO_RUN are next to each other.
+					local actionList = {SCHED_NONE,SCHED_FORCED_GO, SCHED_FORCED_GO_RUN}
+					zombie:SetSchedule( actionList[math.random(1,3)])
+				end
+			end);
 		end
 	end
 	print( "All Entities have initialized\n" )
@@ -28,10 +40,24 @@ hook.Add( "InitPostEntity", "zombie_spawn", function()
 	print( "Initialization hook called" )
 end )
 
+function GM:KeyPress( ply, key )
+    if ( key == IN_USE) then
+		if(table.Count( ply:GetWeapons()) == 0) then
+			ply:Give("weapon_smg1")
+			ply:GiveAmmo( 256,	"smg1")
+		else
+			ply:RemoveAllItems()
+		end
+    end
+end
+
 function GM:EntityTakeDamage( ent, dmgInfo )
 
     local attacker = dmgInfo:GetAttacker()
 	if ent:IsNPC() and ent:GetEnemy() != attacker then
+		hook.Remove("Think", "NPCThink " .. tostring(ent))
+		ent:StopMoving()
+		ent:Give("weapon_alyxgun")
 		-- The zombie will now only attack the player that shot it
 		ent:AddEntityRelationship(attacker, D_HT, 99 )
 		ent:SetEnemy(attacker)
