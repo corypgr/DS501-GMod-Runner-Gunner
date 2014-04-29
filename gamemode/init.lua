4eed55b8d9e2fbefe6acff89c300d6f2bffbeda8
Cory Groom <corypgr@hotmail.com>
2014-04-29 14:54:59 -0500

    moved all the customizable stuff to the top

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -7,34 +7,43 @@ DEFINE_BASECLASS( "gamemode_base" )
 
 --Controls the probablility weight for how long an NPC will stand still
 stand_weight = 10
-
 --Controls the max distance that an NPC will randomly walk to
 walk_weight = 500
+--Type of NPC
+npc_model = "npc_alyx"
+--Type of NPC weapon
+npc_weapon = "weapon_alyxgun"
+--Player model location
+ply_model = "models/alyx.mdl"
+--Type of Player weapon
+ply_weapon = "weapon_smg1"
+ply_weapon_short = "smg1"
+ply_ammo_amount = 256
 
-util.PrecacheModel( "models/alyx.mdl" )
+util.PrecacheModel( ply_model )
 function GM:PlayerSpawn( ply )
 	player_manager.SetPlayerClass( ply, "player_runner" )
-	ply:SetModel("models/alyx.mdl")
+	ply:SetModel( ply_model )
 	BaseClass.PlayerSpawn( self, ply )
 end
 
 function GM:InitPostEntity()
 	for i = 0,3,1 do
 		for k = 0,3,1 do
-			local zombie = ents.Create( "npc_alyx" )
-			zombie:SetPos( Vector(60*i, 60*k, 0 ) )
-			zombie:Spawn()
+			local npc_c = ents.Create( npc_model )
+			npc_c:SetPos( Vector(60*i, 60*k, 0 ) )
+			npc_c:Spawn()
 			-- Make the zombie ignore the players
-			zombie:AddRelationship( "player D_NU 10" )
-			hook.Add("Think", "NPCThink " .. tostring(zombie), function()
-				if(zombie:GetMovementActivity() == -1 and 
-				  (not zombie:IsCurrentSchedule(SCHED_NONE) or math.random(1,stand_weight) == 1)) then
-					local poss = zombie:GetPos()
-					poss:Add(Vector(math.random(-1*walk_weight,walk_weight),math.random(-1*walk_weight,walk_weight),0))
-					zombie:SetLastPosition(poss) 
+			npc_c:AddRelationship( "player D_NU 10" )
+			hook.Add("Think", "NPCThink " .. tostring(npc_c), function()
+				if(npc_c:GetMovementActivity() == -1 and 
+				  (not npc_c:IsCurrentSchedule(SCHED_NONE) or math.random(1,stand_weight) == 1)) then
+					local npc_pos = npc_c:GetPos()
+					npc_pos:Add(Vector(math.random(-1*walk_weight,walk_weight), math.random(-1*walk_weight,walk_weight), 0))
+					npc_c:SetLastPosition(npc_pos) 
 					-- Note that SCHED_FORCED_GO and SCHED_FORCED_GO_RUN are next to each other.
-					local actionList = {SCHED_NONE,SCHED_FORCED_GO, SCHED_FORCED_GO_RUN}
-					zombie:SetSchedule( actionList[math.random(1,3)])
+					local actionList = {SCHED_NONE, SCHED_FORCED_GO, SCHED_FORCED_GO_RUN}
+					npc_c:SetSchedule( actionList[math.random(1,3)] )
 				end
 			end);
 		end
@@ -42,15 +51,12 @@ function GM:InitPostEntity()
 	print( "All Entities have initialized\n" )
 end
 
-hook.Add( "InitPostEntity", "zombie_spawn", function()
-	print( "Initialization hook called" )
-end )
-
 function GM:KeyPress( ply, key )
+	--IN_USE is mapped to e
     if ( key == IN_USE) then
 		if(table.Count( ply:GetWeapons()) == 0) then
-			ply:Give("weapon_smg1")
-			ply:GiveAmmo( 256,	"smg1")
+			ply:Give( ply_weapon )
+			ply:GiveAmmo( ply_ammo_amount,	ply_weapon_short )
 		else
 			ply:RemoveAllItems()
 		end
@@ -58,16 +64,15 @@ function GM:KeyPress( ply, key )
 end
 
 function GM:EntityTakeDamage( ent, dmgInfo )
-
     local attacker = dmgInfo:GetAttacker()
 	if ent:IsNPC() and ent:GetEnemy() != attacker then
 		hook.Remove("Think", "NPCThink " .. tostring(ent))
 		ent:StopMoving()
-		ent:Give("weapon_alyxgun")
-		-- The zombie will now only attack the player that shot it
+		ent:Give( npc_weapon )
+		-- The npc will now only attack the entity that attacked it
 		ent:AddEntityRelationship(attacker, D_HT, 99 )
 		ent:SetEnemy(attacker)
-		-- This lets the zombie find you faster
+		-- This lets the npc find you faster
 		ent:UpdateEnemyMemory( attacker, attacker:GetPos() )
 		print("relationship changed")
 	end
