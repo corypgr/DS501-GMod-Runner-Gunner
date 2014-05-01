1528d55ae24c2d32aa012aff87e68507ec58e2da
Jacob Hanshaw <hanshaw@wisc.edu>
2014-05-01 01:31:47 -0500

    Silliness

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,5 +1,4 @@
-AddCSLuaFile( "cl_init.lua" )
-AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "cl_init.lua" ) AddCSLuaFile( "shared.lua" )
 
 include("shared.lua")
 
@@ -13,20 +12,34 @@ end
 function GM:InitPostEntity()
     for i = 0,10,1 do
         for k = 0,10,1 do
-            local zombie = ents.Create( "npc_alyx" )
+            local zombie = ents.Create( "npc_zombie" )
             zombie:SetPos( Vector(60*i, 60*k, 0 ) )
             zombie:Spawn()
             -- Make the zombie ignore the players
             zombie:AddRelationship( "player D_NU 10" )
             zombie:AddRelationship( "npc_zombie D_NU 10" )
-            zombie:Give("weapon_alyxgun")
-            zombie:SetCurrentWeaponProficiency( WEAPON_PROFICIENCY_POOR);
+          zombie:Give("weapon_crowbar")
+          --  zombie:Give("weapon_alyxgun")
+--            zombie:SetCurrentWeaponProficiency( WEAPON_PROFICIENCY_POOR);
         end
     end
 
     local ent = ents.Create("weapon_crowbar") 
     ent:SetPos(Vector(-10,-10, 0)) 
     ent:Spawn()
+
+    local ent = ents.Create("npc_helicopter") 
+    ent:SetPos(Vector(-50,-50, 200)) 
+    ent:Spawn()
+
+    local ent = ents.Create("npc_ichthyosaur") 
+    ent:SetPos(Vector(-100,-100, 500)) 
+    ent:Spawn()
+
+    local ent = ents.Create("npc_combinegunship") 
+    ent:SetPos(Vector(-150,-150, 750)) 
+    ent:Spawn()
+
 
     print( "All Entities have initialized\n" )
 
@@ -39,16 +52,17 @@ hook.Add( "InitPostEntity", "zombie_spawn", function()
 function GM:EntityTakeDamage( ent, dmgInfo )
 
 local attacker = dmgInfo:GetAttacker()
-    if dmgInfo:GetInflictor():GetName() == "weapon_crowbar" then
+        print(dmgInfo:GetAmmoType())
+
+    if dmgInfo:GetAmmoType() == -1 then
          ent:AddEntityRelationship(attacker, D_LI, 99 )
-    else if ent:IsNPC() and ent:GetEnemy() != attacker then
+    elseif ent:IsNPC() and ent:GetEnemy() != attacker then
         -- The zombie will now only attack the player that shot it
         ent:AddEntityRelationship(attacker, D_HT, 99 )
         ent:SetEnemy(attacker)
         -- This lets the zombie find you faster
         ent:UpdateEnemyMemory( attacker, attacker:GetPos() )
         print("relationship changed")
-    end
     end
 end
 
@@ -68,7 +82,12 @@ local LowList = {
     "item_healthvial",
     "weapon_pistol",
     "weapon_smg1",
-    "weapon_frag"
+    "weapon_frag",
+    "weapon_annabelle",
+    "weapon_extinguisher",
+    "weapon_citizensuitcase",
+    "weapon_citizenpackage",
+    "weapon_brickbat"
 }
 
 local MedList = {
@@ -82,17 +101,19 @@ local HighList = {
     "weapon_357",
     "weapon_crossbow",
     "weapon_rpg",
-    "weapon_physcannon"
+    "weapon_physcannon",
+    "weapon_physgun"
 }
 
 hook.Add("OnNPCKilled", "DropWeaponOnNPCKilled", function(npc, killer)
         local rndweapon = nil
         chance = math.random(1,100)
-        if chance < 50 then return end
-        if chance > 51 && chance < 70 then rndweapon = table.Random(AmmoList) end
-        if chance > 71 && chance < 85 then rndweapon = table.Random(LowList) end
-        if chance > 86 && chance < 95 then rndweapon = table.Random(MedList) end
-        if chance > 96 then rndweapon = table.Random(HighList) end
+        if chance < 50 then return
+        elseif chance > 51 && chance < 70 then rndweapon = table.Random(AmmoList)
+        elseif chance > 71 && chance < 85 then rndweapon = table.Random(LowList)
+        elseif chance > 86 && chance < 95 then rndweapon = table.Random(MedList)
+        else rndweapon = table.Random(HighList) end
+        print("Chance: " .. chance .. " Weapon: " .. rndweapon);
         weapon = ents.Create(rndweapon)
         weapon:SetPos(npc:LocalToWorld(npc:OBBCenter())) 
         weapon:Spawn()
