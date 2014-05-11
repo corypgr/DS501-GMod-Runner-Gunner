0769dd820c20665a2c66dea697183503dc47d359
Cory Groom <corypgr@hotmail.com>
2014-05-11 11:30:16 -0500

    print messages to screen for recruiting npcs

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -8,11 +8,11 @@ DEFINE_BASECLASS( "gamemode_base" )
 
 local function initSpawn( ply ) 
 	if (p1 == nil) then
-		p1 = ply:UserID()
-		print("p1 " .. p1)
+		p1 = ply
+		print("p1 " .. p1:GetName())
 	elseif (p2 == nil) then
-		p2 = ply:UserID()
-		print("p2 " .. p2)
+		p2 = ply
+		print("p2 " .. p2:GetName())
 	end
 end
 hook.Add("PlayerInitialSpawn", "ply_spawn_init", initSpawn)
@@ -70,27 +70,27 @@ function GM:PlayerButtonDown( ply, button )
 		
 		-- all npcs not on teams hate all npcs on teams and vice versa
 		for k, v in pairs(nonTeamNPCs) do
-			for a, b in pairs(p1NPCList) do
-				if(b:IsValid()) then
-					v:AddEntityRelationship(b, D_HT, 99 )
-					b:AddEntityRelationship(v, D_HT, 99 )
+			for i = p1Last, p1First-1, 1 do
+				if(p1NPCList[i]:IsValid()) then
+					v:AddEntityRelationship(p1NPCList[i], D_HT, 99 )
+					p1NPCList[i]:AddEntityRelationship(v, D_HT, 99 )
 				end
 			end
-			for a, b in pairs(p2NPCList) do
-				if(b:IsValid()) then
-					v:AddEntityRelationship(b, D_HT, 99 )
-					b:AddEntityRelationship(v, D_HT, 99 )
+			for i = p2Last, p2First-1, 1 do
+				if(p2NPCList[i]:IsValid()) then
+					v:AddEntityRelationship(p2NPCList[i], D_HT, 99 )
+					p2NPCList[i]:AddEntityRelationship(v, D_HT, 99 )
 				end
 			end
 		end
 		
 		-- the two teams hate each other
-		for k, v in pairs(p1NPCList) do
-			if(v:IsValid()) then
-				for a, b in pairs(p2NPCList) do
-					if(b:IsValid()) then
-						v:AddEntityRelationship(b, D_HT, 99 )
-						b:AddEntityRelationship(v, D_HT, 99 )
+		for k = p1Last, p1First-1, 1 do
+			if(p1NPCList[k]:IsValid()) then
+				for i = p2Last, p2First-1, 1 do
+					if(p2NPCList[i]:IsValid()) then
+						p1NPCList[k]:AddEntityRelationship(p2NPCList[i], D_HT, 99 )
+						p2NPCList[i]:AddEntityRelationship(p1NPCList[k], D_HT, 99 )
 					end
 				end
 			end
@@ -101,48 +101,58 @@ end
 function GM:EntityTakeDamage( ent, dmgInfo )
     local attacker = dmgInfo:GetAttacker()
 	if ent:IsNPC() and dmgInfo:GetDamageType() == DMG_CLUB then -- dmgInfo:GetAmmoType() == -1 then
-		if(attacker:UserID() == p1) then
+		if(attacker == p1) then
 			p1NPCList[p1First] = ent
 			p1First = p1First + 1
 			if((p1First - p1Last) > MaxNPCPerTeam) then
 				p1Last = p1Last + 1
 				print("p1 list full")
 			end
-			removeNPCFromP2List(0, ent)
+			p1:PrintMessage(HUD_PRINTTALK, p1:GetName() .. " recruited an npc")
+			if( p2 != nil ) then
+				p2:PrintMessage(HUD_PRINTTALK, p1:GetName() .. " recruited an npc")
+				removeNPCFromP2List(0, ent, p1)
+			end
 			-- for when you hit the same npc twice
-			removeNPCFromP1List(-1, ent)
-		elseif(attacker:UserID() == p2) then
+			removeNPCFromP1List(-1, ent, p1)
+		elseif(attacker == p2) then
 			p2NPCList[p2First] = ent
 			p2First = p2First + 1
 			if((p2First - p2Last) > MaxNPCPerTeam) then
 				p2Last = p2Last + 1
 				print("p2 list full")
 			end
-			removeNPCFromP1List(0,ent)
+			p1:PrintMessage(HUD_PRINTTALK, p2:GetName() .. " recruited an npc")
+			p2:PrintMessage(HUD_PRINTTALK, p2:GetName() .. " recruited an npc")
+			removeNPCFromP1List(0,ent, p2)
 			-- For when you hit the same npc twice
-			removeNPCFromP2List(-1, ent)
+			removeNPCFromP2List(-1, ent, p2)
 		end
 	end
 end
 
 --endmodifier determines where we stop in the list. 0 is search whole list.
-function removeNPCFromP1List(endModifier, npc) 
+function removeNPCFromP1List(endModifier, npc, ply) 
 	for i = p1Last, (p1First-1+endModifier), 1 do
 		if(p1NPCList[i] == npc) then
 			p1NPCList[i] = p1NPCList[p1Last]
 			p1Last = p1Last + 1
-			print("NPC STOLEN!")
+			p1:PrintMessage(HUD_PRINTTALK, ply:GetName() .. " stole " .. p1:GetName() .. "s npc")
+			if (p2 != nil) then
+				p2:PrintMessage(HUD_PRINTTALK, ply:GetName() .. " stole " .. p1:GetName() .. "s npc")
+			end
 		end
 	end
 end
 
 --endmodifier determines where we stop in the list. 0 is search whole list.
-function removeNPCFromP2List(endModifier, npc) 
+function removeNPCFromP2List(endModifier, npc, ply) 
 	for i = p2Last, p2First-1+endModifier, 1 do
 		if(p2NPCList[i] == npc) then
 			p2NPCList[i] = p2NPCList[p2Last]
 			p2Last = p2Last + 1
-			print("NPC STOLEN!")
+			p1:PrintMessage(HUD_PRINTTALK, ply:GetName() .. " stole " .. p2:GetName() .. "s npc")
+			p2:PrintMessage(HUD_PRINTTALK, ply:GetName() .. " stole " .. p2:GetName() .. "s npc")
 		end
 	end
 end
