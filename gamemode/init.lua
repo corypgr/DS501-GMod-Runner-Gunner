5a60f100396d6f9e30777086c216c83d79b3310c
Jacob Hanshaw <hanshaw@wisc.edu>
2014-05-12 15:20:51 -0500

    Finished Zombie Mode

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -6,85 +6,121 @@ DEFINE_BASECLASS( "gamemode_base" )
 
 function GM:PlayerSpawn( ply )
     player_manager.SetPlayerClass( ply, "player_gunner" )
-    BaseClass.PlayerSpawn( self, ply )
-end
+BaseClass.PlayerSpawn( self, ply )
+    end
 
 function GM:InitPostEntity()
-    for i = 0,10,1 do
-        for k = 0,10,1 do
-            local zombie
-            if k % 4 == 0 then
-            zombie= ents.Create( "npc_poisonzombie" )
-            elseif k % 2 == 0 then
-             zombie= ents.Create( "npc_fastzombie" )
-            else  
-            zombie= ents.Create( "npc_zombie" )
-            end 
-            zombie:SetPos( Vector(60*i, 60*k, 0 ) )
-            zombie:Spawn()
-            -- Make the zombie ignore the players
-            zombie:AddRelationship( "player D_HT 10" )
-            zombie:AddRelationship( "npc_zombie D_NU 10" )
-          --  zombie:Give("weapon_alyxgun")
---            zombie:SetCurrentWeaponProficiency( WEAPON_PROFICIENCY_POOR);
-        end
+
+    local x = 0
+    local y = 0
+    local z = 0
+
+    for i = 5,10,1 do
+    for k = 5,10,1 do
+    local zombie
+    if k % 4 == 0 then
+    zombie= ents.Create( "npc_poisonzombie" )
+    elseif k % 2 == 0 then
+    zombie= ents.Create( "npc_fastzombie" )
+    else  
+    zombie= ents.Create( "npc_zombie" )
+    end 
+    zombie:SetPos( Vector(x + 60*i, y + 60*k, z ) )
+    zombie:Spawn()
+    zombie:AddRelationship( "player D_HT 99" )
+    end
+    end
+
+    x = -600
+    y = 1600
+
+    for i = 0,5,1 do
+    for k = 0,5,1 do
+    local zombie
+    if k % 4 == 0 then
+    zombie= ents.Create( "npc_poisonzombie" )
+    elseif k % 2 == 0 then
+    zombie= ents.Create( "npc_fastzombie" )
+    else  
+    zombie= ents.Create( "npc_zombie" )
+    end 
+    zombie:SetPos( Vector(x + 60*i, y + 60*k, z ) )
+    zombie:Spawn()
+    zombie:AddRelationship( "player D_HT 99" )
+    end
+    end
+
+    x = -600
+    y = 1000
+
+    for i = 0,5,1 do
+    for k = 0,5,1 do
+    local zombie
+    if k % 4 == 0 then
+    zombie= ents.Create( "npc_poisonzombie" )
+    elseif k % 2 == 0 then
+    zombie= ents.Create( "npc_fastzombie" )
+    else  
+    zombie= ents.Create( "npc_zombie" )
+    end 
+    zombie:SetPos( Vector(x + 60*i, y + 60*k, z ) )
+    zombie:Spawn()
+    zombie:AddRelationship( "player D_HT 99" )
+    end
+    end
+
+    x = 880
+    y = -450
+    z = 1300
+
+    for i = 0,5,1 do
+    for k = 0,5,1 do
+    local zombie
+    if k % 4 == 0 then
+    zombie= ents.Create( "npc_poisonzombie" )
+    elseif k % 2 == 0 then
+    zombie= ents.Create( "npc_fastzombie" )
+    else  
+    zombie= ents.Create( "npc_zombie" )
+    end 
+    zombie:SetPos( Vector(x + 60*i, y + 60*k, z ) )
+    zombie:Spawn()
+    zombie:AddRelationship( "player D_HT 99" )
+    end
     end
 
     local ent = ents.Create("weapon_crowbar") 
     ent:SetPos(Vector(-20,-20, 0)) 
     ent:Spawn()
 
-    local ent = ents.Create("npc_combinegunship") 
-    ent:SetPos(Vector(-150,-150, 750)) 
-    ent:Spawn()
-
-
     print( "All Entities have initialized\n" )
 
-end
-
-hook.Add( "InitPostEntity", "zombie_spawn", function()
-        print( "Initialization hook called" )
-        end )
-
-function GM:EntityTakeDamage( ent, dmgInfo )
-
-local attacker = dmgInfo:GetAttacker()
-        print(dmgInfo:GetAmmoType())
-
-    if dmgInfo:GetDamageType() == DMG_CLUB then -- dmgInfo:GetAmmoType() == -1 then
-         ent:AddEntityRelationship(attacker, D_LI, 99 )
-    elseif ent:IsNPC() and ent:GetEnemy() != attacker then
-        -- The zombie will now only attack the player that shot it
-        ent:AddEntityRelationship(attacker, D_HT, 99 )
-        ent:SetEnemy(attacker)
-        -- This lets the zombie find you faster
-        ent:UpdateEnemyMemory( attacker, attacker:GetPos() )
-        print("relationship changed")
     end
-end
 
-local AmmoList = {
-    "item_ammo_pistol",
-    "item_ammo_smg1",
-    "item_ammo_ar2",
-    "item_ammo_357",
-    "item_ammo_crossbow",
-    "item_box_buckshot",
-    "item_rpg_round"
+    local AmmoList = {
+        "item_ammo_pistol",
+        "item_ammo_smg1",
+        "item_ammo_ar2",
+        "item_ammo_357",
+        "item_ammo_crossbow",
+        "item_box_buckshot",
+        "item_rpg_round"
+    }
+
+local MedicalList = {
+    "item_battery",
+    "item_healthkit",
+    "item_healthvial"
 }
 
 local LowList = {
-    "item_battery",
-    "item_healthkit",
-    "item_healthvial",
-    "weapon_pistol",
-    "weapon_frag"
+    "weapon_ar2",
+    "weapon_smg1",
+    "weapon_pistol"
 }
 
 local MedList = {
-    "weapon_smg1",
-    "weapon_ar2",
+    "weapon_frag",
     "weapon_shotgun",
     "item_ammo_ar2_altfire",
     "item_ammo_smg1_grenade"
@@ -98,14 +134,32 @@ local HighList = {
 
 hook.Add("OnNPCKilled", "DropWeaponOnNPCKilled", function(npc, killer)
         local rndweapon = nil
-        chance = math.random(1,100)
-        if chance < 55 then return
-        elseif chance > 56 && chance < 70 then rndweapon = table.Random(AmmoList)
-        elseif chance > 71 && chance < 85 then rndweapon = table.Random(LowList)
-        elseif chance > 86 && chance < 95 then rndweapon = table.Random(MedList)
-        else rndweapon = table.Random(HighList) end
+        local chance = math.random(1,100)
+        if chance <= 55 then return
+        elseif chance >= 56 && chance <= 70 then rndweapon = table.Random(AmmoList)
+        elseif chance >= 71 && chance <= 75 then rndweapon = table.Random(MedicalList)
+        elseif chance >= 76 && chance <= 87 then rndweapon = table.Random(LowList)
+        elseif chance >= 88 && chance <= 98 then rndweapon = table.Random(MedList)
+        else   rndweapon = table.Random(HighList) end
         print("Chance: " .. chance .. " Weapon: " .. rndweapon);
         weapon = ents.Create(rndweapon)
         weapon:SetPos(npc:LocalToWorld(npc:OBBCenter())) 
         weapon:Spawn()
         end)
+
+function getNPCCounts(ply) 
+    local count = 0
+    local allNPCs = ents.FindByClass("npc_*")
+    for k, v in pairs(allNPCs) do
+    if(v:IsValid()) then
+    count = count + 1
+    end
+    end
+    ply:PrintMessage(HUD_PRINTTALK, "NPC Counts:".. count)
+    end
+
+function GM:PlayerButtonDown( ply, button )
+    if( button == KEY_C ) then
+        getNPCCounts(ply)
+    end
+end
